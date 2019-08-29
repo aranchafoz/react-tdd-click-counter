45dfaf37e1b274fe841f41d6f61164f7d6e75ce5
morenocantoj <morenocantoj@gmail.com>
2019-08-29 20:20:01 +0200

    Counter which can be incremented or decremented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      decrementError: false
+    }
+  }
+
+  handleDecrement() {
+    const { counter } = this.state;
+    if(counter > 0) {
+      this.setState({counter: counter - 1});
+    } else {
+      this.setState({decrementError: true});
+    }
+  }
+
+  handleIncrement() {
+    const { counter } = this.state;
+    this.setState({counter: counter + 1, decrementError: false});
+  }
+
+  render() {
+    const { counter, decrementError } = this.state;
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently {counter}</h1>
+        <div className="buttons-container">
+          <button
+            data-test="decrement-button"
+            onClick={() => this.handleDecrement()}
+            >
+              Decrement counter
+          </button>
+          <button
+            data-test="increment-button"
+            onClick={() => this.handleIncrement()}
+            >
+              Increment counter
+          </button>
+        </div>
+        {decrementError &&
+          <h3 data-test="error-display">The counter can't go below zero</h3>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
